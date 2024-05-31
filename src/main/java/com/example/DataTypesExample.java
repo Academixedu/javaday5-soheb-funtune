51bcd00e8e89b2cfe714cff1611627a5f2d5cbe1
soheb-funtune <sohebs5050@gmail.com>
2024-05-31 10:06:00 -0700

    assignments day 5

--- src/main/java/com/example/DataTypesExample.java
+++ src/main/java/com/example/DataTypesExample.java
@@ -2,10 +2,10 @@ package com.example;
 
 public class DataTypesExample {
     public static void main(String[] args) {
-        int num = _____;
-        char letter = '_____';
-        boolean isJavaFun = _____;
-        double price = _____;
+        int num = 33;
+        char letter = 'M';
+        boolean isJavaFun = true;
+        double price = 300.33d;
         System.out.println("Number: " + num);
         System.out.println("Letter: " + letter);
         System.out.println("Is Java Fun: " + isJavaFun);
